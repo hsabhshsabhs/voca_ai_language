8b72822bf71c8376e26f9e4e7735d881c063eea0
hsabhshsabhs <85287557+hsabhshsabhs@users.noreply.github.com>
2026-03-20 11:28:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -337,7 +337,7 @@ async def telegram_webhook(request: Request):
         async with aiohttp.ClientSession() as session:
             await session.post(url, json={
                 "chat_id": chat_id, 
-                "animation": "https://raw.githubusercontent.com/hsabhshsabhs/voca_ai_language/main/https://lingvoai_preview.gif",
+                "animation": "https://raw.githubusercontent.com/hsabhshsabhs/voca_ai_language/main/lingvoai_preview.gif",
                 "text": welcome_text, 
                 "parse_mode": "HTML", 
                 "disable_web_page_preview": False,
